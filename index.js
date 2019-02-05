ff8020b31cafed7294c08e074292f1f4abc49d5b
Michael Hicks <hicks74@purdue.edu>
2019-02-05 15:35:43 -0500

    Server Listening on local port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000
-const io = requre('socket.io')();
+const io = require('socket.io')();
 
 
 app.get('/', (req, res) => res.send('Hello World'))
