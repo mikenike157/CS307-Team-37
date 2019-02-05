4ace530424a67cd68ff2d0090e5f1ed69e02be2e
Michael Hicks <hicks74@purdue.edu>
2019-02-05 15:29:58 -0500

    Server functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000
+const io = requre('socket.io')();
+
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log('Listening on port ${port}'))
