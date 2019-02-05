27a1c189d79a0c63ad0b7a71706069737f3fc73b
Michael Hicks <hicks74@purdue.edu>
2019-02-05 15:59:42 -0500

    Socket.io connection counting

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const port = process.env.PORT || 80;
 const io = require('socket.io')();
 const path = require('path')
 
+var numPeople = 0;
 
+app.get('/', (req, res) => {
+  res.send('Hello World'))
+  count++;
+  console.log(count);
+}
 
-
-app.get('/', (req, res) => res.send('Hello World'))
+io.on('connection', function(socket) {
+  console.log("User connected");
+  numPeople++;
+  console.log(numPeople);
+  socket.on('disconnect', function() {
+    console.log("User disconnected");
+    numPeople--;
+    console.log(numPeople);
+  });
+});
 
 app.listen(port)
