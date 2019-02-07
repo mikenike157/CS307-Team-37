fa7aa895795858928587fa937a4a876b7583ffd1
Michael Hicks <hicks74@purdue.edu>
2019-02-06 20:29:37 -0500

    Room counters and limits

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const port = process.env.PORT || 80;
 const io = require('socket.io')();
 const path = require('path')
 
-var numPeople = 0;
+var roomArray = new Array(100)
+
+function Gameroom(roomName, numPlayers) {
+  this.roomName = roomName;
+  this.numPlayers = numPlayers;
+}
+
+for (var i = 0; i < 100; i++) {
+  var room = new Gameroom("Room " + i, 0);
+  roomArray[i] = room;
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    var x = 0;
+    while (roomArray[x].numPlayers > 7) {
+      console.log(roomArray[x].numPlayers)
+      x++;
+    }
+    res.send("Hello World")
+    roomArray[x].numPlayers++;
+    console.log(roomArray[x].roomName + ": " + roomArray[x].numPlayers);
 });
 
 io.on('connection', function(socket) {
