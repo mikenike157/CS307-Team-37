87b1aaebb5f44b8c4590e4c06356b8df2330419d
Michael Hicks <hicks74@purdue.edu>
2019-02-16 11:32:09 -0500

    socket io preparation and experimentation

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 80;
+const path = require('path')
+const sio = require('socket.io')
 const pg = require('pg');
 const pool = new pg.Pool({
   user: 'uvqmfjuwtlhurl',
@@ -9,55 +14,34 @@ const pool = new pg.Pool({
   port: '5432',
   ssl: true
 });
-const port = process.env.PORT || 80;
-const io = require('socket.io')();
-const path = require('path')
 
-var roomArray = new Array(100)
+const server = express()
+  .use((req, res) => res.sendFile(path.join(__dirname, 'chat.html')))
+  .listen(port, () => console.log(`Listening on ${ port }`));
 
-function Gameroom(roomName, numPlayers) {
-  this.roomName = roomName;
-  this.numPlayers = numPlayers;
-}
+const io = sio(server);
 
-for (var i = 0; i < 100; i++) {
-  var room = new Gameroom("Room " + i, 0);
-  roomArray[i] = room;
-}
+var roomArray = new Array(101)
 
-app.get('/', (req, res) => {
-    var x = 0;
-    while (roomArray[x].numPlayers > 7) {
-      console.log(roomArray[x].numPlayers)
-      x++;
-      if (x == 100) {
-        res.send("Sorry all rooms are full")
-        return;
-      }
-    }
-    res.send("You are player " + (roomArray[x].numPlayers + 1) + " in room " + roomArray[x].roomName);
-    roomArray[x].numPlayers++;
-    console.log(roomArray[x].roomName + ": " + roomArray[x].numPlayers);
+app.get('/', function(req, res) => {
+  res.sendFile(__dirname + 'chat.html');
 });
 
-app.get('/login', (req, res) => {
-  var username = request.query.username;
-  var password = request.query.password;
-  pool.query('Select * from users where username = ${username}', (error, results)=> {
-
-  });
+app.get('room1' function(req, res) => {
 
 })
-/*
+
+app.get('room2' function(req, res) => {
+  
+})
+
 io.on('connection', function(socket) {
-  console.log("User connected");
-  numPeople++;
-  console.log(numPeople);
-  socket.on('disconnect', function() {
-    console.log("User disconnected");
-    numPeople--;
-    console.log(numPeople);
-  });
+  console.log('connect');
+  socket.on('disconnect', () => console.log('disconnect'));
 });
-*/
-app.listen(port)
+
+app.get()
+
+
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
