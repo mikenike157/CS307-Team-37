fe2c1379e21e2243832f5e2ebc7810a429ae0938
Michael Hicks <hicks74@purdue.edu>
2019-02-17 15:39:26 -0500

    Chat Attempt

--- index.js
+++ index.js
@@ -27,21 +27,45 @@ app.get('/', function(req, res) => {
   res.sendFile(__dirname + 'chat.html');
 });
 
-app.get('room1' function(req, res) => {
+var usernames = {};
 
-})
+// rooms which are currently available in chat
+var rooms = ['room1'];
 
-app.get('room2' function(req, res) => {
-  
-})
+io.sockets.on('connection', function (socket) {
 
-io.on('connection', function(socket) {
-  console.log('connect');
-  socket.on('disconnect', () => console.log('disconnect'));
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username){
+		// store the username in the socket session for this client
+		socket.username = username;
+		// store the room name in the socket session for this client
+		socket.room = 'room1';
+		// add the client's username to the global list
+		usernames[username] = username;
+		// send client to room 1
+		socket.join('room1');
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		// echo to room 1 that a person has connected to their room
+		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.emit('updaterooms', rooms, 'room1');
+	});
+
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function (data) {
+		// we tell the client to execute 'updatechat' with 2 parameters
+		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+	});
+
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.leave(socket.room);
+	});
 });
-
-app.get()
-
-
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
