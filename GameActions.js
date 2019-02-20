565188d581b6b10f19b04d02d62f2e7dce96f085
teressatuan <43707773+teressatuan@users.noreply.github.com>
2019-02-20 11:20:37 -0500

    Updated playerRaise function

--- GameActions.js
+++ GameActions.js
@@ -2,19 +2,18 @@ var SUITS = new Array("Spades", "Hearts", "Clubs", "Diamonds")
 var VALUES = new Array("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
 var STATES = new Array("NOTREADY", "READY", "FOLDED")
 
-    // Length N array of Player objects
-    // Each Player object contains chip information, current state
+// Length N array of Player objects
+// Each Player object contains chip information, current state
 var allPlayers = [];
-
-    // Legnth 2N+5 array of unique integers
-    // Syntax gameActions.gameInfo.array = [];
-var numbers = [];
+var numbers = []; // 2N+5 unique random integers on [0, 51] 
 var playerCards = [[],[], [], [], [], [], [], []]; // maximum of eight players
 var playerOrder = []; // allPlayers[playerOrder[0]] to get 0th player
 var tableCards = [];
-var playerChips = [];
 
-    // Track current bet and pot
+var playerChips = [];
+var playerStates = [];
+
+// Track current bet and pot
 var currentBet;
 var pot;
 var N;
@@ -26,20 +25,12 @@ var N;
 
     this.startGame = function() // change to pass game object instead, gameInfo
     {
-
         N = 8;
         currentBet = 0;
         pot = 0;
 
         // Deal
-        var m = 0;
-        /*while (m < 2*N+5) {
-          k = Math.floor(Math.random()*51);
-          if (numbers.findIndex(value(k)) == -1) {
-            numbers[m] = k;
-            m++;
-          }
-        }*/
+        var m = 0; // 1. Random numbers
         for (var i = 0; i < (2*N+5); i++)
         {
           var k = Math.floor(Math.random()*51);
@@ -55,22 +46,22 @@ var N;
             else { j--; }
           }
         }
-        var k = 0;
+        var k = 0; // 2. Assign to players
         for (var i = 0; i < (N*2); i+=2)
         {
-
             playerCards[k][0] = numbers[i];
             playerCards[k][1] = numbers[i+1];
-
             k++;
         }
-        k=0
+        k=0 // 3. Assign table cards 
         for (var i = (2*N); i < (2*N+5); i++)
         {
             tableCards[k] = numbers[i];
             k++
         }
         return [playerCards, tableCards];
+        // Could use playerRaise for blinds, take amount as parameter and numerical player index [0, 1]  
+        
         // Do other stuff here
         // Ante, blinds? Consider later
         // 1. Assign random order for players/
@@ -90,6 +81,23 @@ var N;
             // Array of player cards (first 2N cards)
             // Array of table cards (last 5 cards)
     }
+
+    // TODO: Test this
+    playerRaise: function(pIndex, amount)
+    {
+        // Increase current bet
+        if (playerChips[pIndex] >= amount)
+        {
+            playerChips[pIndex] -= amount;
+            playerStates[pIndex] = "READY";
+            pot += amount;
+            currentBet = amount;
+            return 1;
+        }
+        // Fail to raise 
+        return 0; 
+    },
+        
 /*
     // TODO: Inner game loop
 
