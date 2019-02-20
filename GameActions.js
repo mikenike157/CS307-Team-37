6ff37753bc72d8797c0a860c045fbfdc0cde428a
Kyarei <uruwi@protonmail.com>
2019-02-20 15:01:11 -0500

    Fix formatting

--- GameActions.js
+++ GameActions.js
@@ -1,109 +1,101 @@
-var SUITS = new Array("Spades", "Hearts", "Clubs", "Diamonds")
-var VALUES = new Array("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
-var STATES = new Array("NOTREADY", "READY", "FOLDED")
+const SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"];
+const VALUES = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+const STATES = ["NOTREADY", "READY", "FOLDED"];
 
 // Length N array of Player objects
 // Each Player object contains chip information, current state
-var allPlayers = [];
-var numbers = []; // 2N+5 unique random integers on [0, 51]
-var playerCards = [[],[], [], [], [], [], [], []]; // maximum of eight players
-var tableCards = [];
+let allPlayers = [];
+let numbers = []; // 2N+5 unique random integers on [0, 51]
+let playerCards = [[], [], [], [], [], [], [], []]; // maximum of eight players
+let tableCards = [];
 
-var playerOrder = []; // not currently used; assign N values on [0, N-1]
-var playerChips = [];
-var playerStates = [];
-var lastBet = [];
+let playerOrder = []; // not currently used; assign N values on [0, N-1]
+let playerChips = [];
+let playerStates = [];
+let lastBet = [];
 
 // Track current bet and pot
-var currentBet;
-var pot;
-var N;
+let currentBet;
+let pot;
+let N;
 
 class Player {
-    constructor(playerID, order, chips)
-    {
-        this.playerID = playerID;
-        this.order = order;
-        this.lastBet = 0;
-        this.playerState = "NOTREADY";
-        this.playerChips = chips;
-        this.cards = [-1, -1];
-    }
+  constructor(playerID, order, chips) {
+    this.playerID = playerID;
+    this.order = order;
+    this.lastBet = 0;
+    this.playerState = "NOTREADY";
+    this.playerChips = chips;
+    this.cards = [-1, -1];
+  }
 }
 
 
-    this.addPlayer = function(socketid, n) {
-      var player = new Player(socketid, n, 100);
-      //allPlayers[n] = player;
-      return player;
+this.addPlayer = function(socketid, n) {
+  let player = new Player(socketid, n, 100);
+  //allPlayers[n] = player;
+  return player;
+};
+
+
+this.testPlayer = function() {
+  let player0 = new Player(0, 0, 100);
+  return player0;
+};
+
+this.startGame = function() // change to pass game object instead, gameInfo
+{
+  const N = 2;
+  let currentBet = 0;
+  let pot = 0;
+  const DEFAULT_CHIPS = 100;
+  // Deal
+  let m = 0; // 1. Random numbers
+  for (let i = 0; i < (2*N+5); i++) {
+    let k = Math.floor(Math.random()*51);
+    numbers[i] = k;
+    let j = i-1; // check for and eliminate repeats
+    while (j >= 0) {
+      if (numbers[i] == numbers[j]) {
+        numbers[i] = (numbers[i]+1) % 52;
+        j = i-1;
+      } else {
+        j--; 
+      }
     }
+  }
+  let k = 0; // 2. Assign to players
+  for (let i = 0; i < (N*2); i+=2) {
+    playerCards[k][0] = numbers[i];
+    playerCards[k][1] = numbers[i+1];
+    k++;
+  }
+  k = 0; // 3. Assign table cards
+  for (let i = (2*N); i < (2*N+5); i++) {
+    tableCards[k] = numbers[i];
+    k++;
+  }
+  // Later: Assign random order for players
 
+  // 0th player = dealer, 1st = small blind, 2nd = big blind
+  // Could use playerRaise for blinds, take amount as parameter and numerical player index [0, 1]
 
-    this.testPlayer = function()
-    {
-        var player0 = new Player(0, 0, 100);
-        return player0;
-    }
+  return [playerCards, tableCards];
+};
 
-    this.startGame = function() // change to pass game object instead, gameInfo
-    {
-        N = 2;
-        currentBet = 0;
-        pot = 0;
-        defaultChips = 100;
-        // Deal
-        var m = 0; // 1. Random numbers
-        for (var i = 0; i < (2*N+5); i++)
-        {
-          var k = Math.floor(Math.random()*51);
-          numbers[i] = k;
-          var j = i-1; // check for and eliminate repeats
-          while (j >= 0)
-          {
-            if (numbers[i] == numbers[j])
-            {
-              numbers[i] = (numbers[i]+1) % 52;
-              j = i-1;
-            }
-            else { j--; }
-          }
-        }
-        var k = 0; // 2. Assign to players
-        for (var i = 0; i < (N*2); i+=2)
-        {
-            playerCards[k][0] = numbers[i];
-            playerCards[k][1] = numbers[i+1];
-            k++;
-        }
-        k = 0; // 3. Assign table cards
-        for (var i = (2*N); i < (2*N+5); i++)
-        {
-            tableCards[k] = numbers[i];
-            k++;
-        }
-        // Later: Assign random order for players
-
-        // 0th player = dealer, 1st = small blind, 2nd = big blind
-        // Could use playerRaise for blinds, take amount as parameter and numerical player index [0, 1]
-
-        return [playerCards, tableCards];
-    }
-
-    // TODO #1: Test this. Raise /to/ amount
-    this.playerRaise = function(pIndex, amount)
-    {
-        // Increase current bet
-        if (playerChips[pIndex] >= amount)
-        {
-            playerChips[pIndex] -= amount;
-            playerStates[pIndex] = "READY";
-            pot += amount;
-            currentBet = amount;
-            return 1;
-        }
-        // Fail to raise
-        return 0;
-    }
+// TODO #1: Test this. Raise /to/ amount
+this.playerRaise = function(pIndex, amount) {
+  // Increase current bet
+  if (playerChips[pIndex] >= amount) {
+    playerChips[pIndex] -= amount;
+    playerStates[pIndex] = "READY";
+    pot += amount;
+    currentBet = amount;
+    return 1;
+  }
+  // Fail to raise
+  return 0;
+};
 
 /*
     // TODO: Inner game loop
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const game = require('./GameActions.js');
-const validator = require('validator');
-const path = require('path')
-const sio = require('socket.io')
-const pg = require('pg');
-const hf = require('./handFinder.js')
+const express = require("express");
+const game = require("./GameActions.js");
+const validator = require("validator");
+const path = require("path");
+const sio = require("socket.io");
+const pg = require("pg");
+const hf = require("./handFinder.js");
 
 
 const port = process.env.PORT || 80;
@@ -21,110 +21,109 @@ const pool = new pg.Pool({
 });
 
 const server = express()
-  .use((req, res) => res.sendFile(path.join(__dirname, 'chat.html')))
+  .use((req, res) => res.sendFile(path.join(__dirname, "chat.html")))
   .listen(port, () => console.log(`Listening on ${ port }`));
 
 const io = sio(server);
 
 
-var players = [];
+let players = [];
 
-var roomArray = new Array(101)
+let roomArray = new Array(101);
 
-var usernames = {};
+let usernames = {};
 
 // rooms which are currently available in chat
-var rooms = ['room1'];
-var availSockets = [];
-io.sockets.on('connection', function (socket) {
+let rooms = ["room1"];
+let availSockets = [];
+io.sockets.on("connection", function (socket) {
 
-	// when the client emits 'adduser', this listesns and executes
-	socket.on('adduser', function(username){
-		// store the username in the socket session for this client
-    var player = game.addPlayer(socket.id, availSockets.length);
+  // when the client emits 'adduser', this listesns and executes
+  socket.on("adduser", function(username){
+    // store the username in the socket session for this client
+    let player = game.addPlayer(socket.id, availSockets.length);
     players[availSockets.length] = player;
     availSockets.push(socket.id);
     username = validator.escape(username);
-		socket.username = username;
-		// store the room name in the socket session for this client
-		socket.room = 'room1';
-		// add the client's username to the global list
-		usernames[username] = username;
-    console.log()
+    socket.username = username;
+    // store the room name in the socket session for this client
+    socket.room = "room1";
+    // add the client's username to the global list
+    usernames[username] = username;
+    console.log();
     console.log(player);
-		// send client to room 1
-		socket.join('room1');
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'room1');
-	});
+    // send client to room 1
+    socket.join("room1");
+    // echo to client they've connected
+    socket.emit("updatechat", "SERVER", "you have connected to room1");
+    // echo to room 1 that a person has connected to their room
+    socket.broadcast.to("room1").emit("updatechat", "SERVER", username + " has connected to this room");
+    socket.emit("updaterooms", rooms, "room1");
+  });
 
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-    var parsed = data.split(" ");
+  // when the client emits 'sendchat', this listens and executes
+  socket.on("sendchat", function (data) {
+    // we tell the client to execute 'updatechat' with 2 parameters
+    let parsed = data.split(" ");
     if (data == "/start") {
-      var cards = game.startGame();
-      var playerCards = cards[0];
-      var tableCards = cards[1];
+      let cards = game.startGame();
+      let playerCards = cards[0];
+      let tableCards = cards[1];
       console.log(playerCards);
       console.log(tableCards);
-      var handRanks = [];
-      for (var i = 0; i < playerCards.length; i++) {
-        var hand = hf.finalhand(playerCards[i], tableCards)
-        var matchArray = hf.match(hand);
-        var handArray0 = hf.kinds(matchArray);
-        var handArray1 = hf.flushAndStraight(hand,matchArray);
-        if(handArray0[0]>handArray1[0]){
+      let handRanks = [];
+      for (let i = 0; i < playerCards.length; i++) {
+        let hand = hf.finalhand(playerCards[i], tableCards);
+        let matchArray = hf.match(hand);
+        let handArray0 = hf.kinds(matchArray);
+        let handArray1 = hf.flushAndStraight(hand, matchArray);
+        if (handArray0[0]>handArray1[0]){
           handRanks.push(handArray0);
-        }else{
+        } else {
           handRanks.push(handArray1);
         }
       }
-      var winner = hf.findWinner(handRanks)
-      for (var i = 0; i < availSockets.length; i++) {
+      let winner = hf.findWinner(handRanks);
+      for (let i = 0; i < availSockets.length; i++) {
         players[i].cards = playerCards[i];
-        io.to(availSockets[i]).emit('updatechat', "Your Cards: ", playerCards[i]);
+        io.to(availSockets[i]).emit("updatechat", "Your Cards: ", playerCards[i]);
       }
-      console.log(players)
+      console.log(players);
 
-      var newdata = validator.escape(data)
+      let newdata = validator.escape(data);
       //io.sockets.in(socket.room).emit('updatechat', "Player Cards: ", playerCards);
       //io.sockets.in(socket.room).emit('updatechat', "Table Cards: ", tableCards)
       return;
     }
 
     if (parsed[0] == "/bet") {
-      var newdata = validator.escape(data)
-      io.sockets.in(socket.room).emit('updatechat', socket.username, "Congrats youve bet " + parsed[1] + " chips.")
-    }
-    else {
+      let newdata = validator.escape(data);
+      io.sockets.in(socket.room).emit("updatechat", socket.username, "Congrats youve bet " + parsed[1] + " chips.");
+    } else {
       if (data != "") {
-        var newdata = validator.escape(data)
+        let newdata = validator.escape(data);
 
-        io.sockets.in(socket.room).emit('updatechat', socket.username, newdata);
+        io.sockets.in(socket.room).emit("updatechat", socket.username, newdata);
       }
     }
-	});
+  });
 
 
-	// when the user disconnects.. perform this
-	socket.on('disconnect', function(){
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// update list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-		socket.leave(socket.room);
-	});
+  // when the user disconnects.. perform this
+  socket.on("disconnect", function(){
+    // remove the username from global usernames list
+    delete usernames[socket.username];
+    // update list of users in chat, client-side
+    io.sockets.emit("updateusers", usernames);
+    // echo globally that this client has left
+    socket.broadcast.emit("updatechat", "SERVER", socket.username + " has disconnected");
+    socket.leave(socket.room);
+  });
 });
 
 function getSockets(room) { // will return all sockets with room name
   return Object.entries(io.sockets.adapter.rooms[room] === undefined ?
-  {} : io.sockets.adapter.rooms[room].sockets )
+    {} : io.sockets.adapter.rooms[room].sockets )
     .filter(([id, status]) => status) // get only status = true sockets
-    .map(([id]) => io.sockets.connected[id])
+    .map(([id]) => io.sockets.connected[id]);
 }
