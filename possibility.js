97b3ea9f7ac686062a887862e2787393c23b5150
Darono <atramm@purdue.edu>
2019-03-19 11:45:54 -0400

    Add files via upload

--- possibility.js
+++ possibility.js
@@ -5,7 +5,19 @@ function possibility(tableArray,matchArray,numTableCards) {
 	var secondhighmatch = 0;
 	var nCardsStraight = 0;
 	var flushcount = 0;
-	var cardsleft = 7-numTableCards;
+	var cardsleft = 7-numTableCards;	
+	
+	var fourCounter = 0;
+	var fourHigh = -1;
+	  
+	var tripleCounter = 0;
+	var tripleHigh = -1;
+	  
+	var doubleCounter = 0;
+	var doubleCountArray = [-1,-1];
+	  
+	var fullHouse = false;
+	var twoPair = false; 
 	
 	if(numTableCards == 5){
 		return -1;
@@ -22,13 +34,17 @@ function possibility(tableArray,matchArray,numTableCards) {
 		  }
 	 }
 	 //2pair
-	 if((cardsleft >= 2) || (secondhighmatch == 2) || (currentHighMatch == 2 && cardsleft >= 1)){
+	 if((cardsleft >= 2) || (secondhighmatch == 2) || (currentHighMatch >= 2 && cardsleft >= 1)){
 		 rArray[2] = 1;		 
+	 }else{
+		 rArray[2] = 0;
 	 }
 	 
 	 //3kind
 	 if((cardsleft + currentHighMatch) >= 3){
 		 rArray[3] = 1;
+	 }else{
+		 rArray[3] = 0;
 	 }
 	 
 	 //straight
@@ -75,13 +91,18 @@ function possibility(tableArray,matchArray,numTableCards) {
 	  
 	  if((nCardsStraight + cardsleft) >= 5){
 		  rArray[4] = 1;
-	  } 
+	  }
+	  
+
+	  if(rArray[4] != 1){
+		  rArray[4] = 0;
+	  }
 	  
 	  //flush
 	  for (var i = 0; i < tableArray.length; i++) {
-		  for(var j = 0; j < tableArray.length; j++){
+		  for(var j = 0; j < tableArray[i].length; j++){
 			  
-			  if(x[i][j] == 1){
+			  if(tableArray[i][j] == 1){
 				  flushcount = flushcount + 1;		
 			  }
 			  if((flushcount + cardsleft) >= 5){
@@ -92,15 +113,23 @@ function possibility(tableArray,matchArray,numTableCards) {
 		  flushcount = 0;
 	  }
 
+	  if(rArray[5] != 1){
+		  rArray[5] = 0;
+	  }
+
 	  //fullhouse
-	  if((cardsleft >= 3) || (secondhighmatch == 2 && currentHighMatch == 3) || (currentHighMatch >= 3 && cardsleft >= 1) || (currentHighMatch >= 2 && cardsleft >= 2) || (secondhighmatch>=2 ||cardsleft >= 1)){
+	  if((cardsleft >= 3) || (secondhighmatch == 2 && currentHighMatch == 3) || (currentHighMatch >= 3 && cardsleft >= 1) || (currentHighMatch >= 2 && cardsleft >= 2) || (secondhighmatch>=2 && cardsleft >= 1)){
 			 rArray[6] = 1;		 
+	  }else{
+		  rArray[6] = 0;
 	  }
 	 
 	 //4kind
 	 if((cardsleft + currentHighMatch) >= 4){
 		 rArray[7] = 1;
-	 }
+	 }else{
+		  rArray[7] = 0;
+	  }
 	
 	 
 	  //Straight flush 
@@ -156,6 +185,145 @@ function possibility(tableArray,matchArray,numTableCards) {
 		 }
 		  
 	 }
+	  if(rArray[8] != 1){
+		  rArray[8] = 0;
+	  }
+	  
+	  
+	  	  
+	  for(var k = 0; k < matchArray.length; k++){
+		  		  
+		  //How many 4 of a kinds
+		  if(matchArray[k] == 4){ 
+			  fourCounter = fourCounter + 1;
+			  fourHigh = k;
+		  }
+		  
+		//How many 3 of a kinds
+		  if(matchArray[k] == 3){
+			  tripleCounter = tripleCounter + 1;
+			  tripleHigh = k;
+		  }
+		  
+		//How many pairs
+		  if(matchArray[k] == 2){
+			  
+			  if(doubleCounter == 0){
+				  doubleCountArray[0] = k;
+			  }
+			  
+			  if(doubleCounter > 0){
+				  doubleCountArray[1] = doubleCountArray[0];
+				  doubleCountArray[0] = k;
+			  }		  
+				  
+			  doubleCounter = doubleCounter + 1;			  
+		  }			  
+	  }
+	  
+	  //Is there a Full House
+	  if(tripleCounter >= 1 && doubleCounter >= 1){
+		  fullHouse = true;
+		  fullHouseArray = [tripleHigh,doubleCountArray[0]]
+	  }
+	  
+	  //Is there a two Pair
+	  if(doubleCounter >= 2 && !fullHouse){
+		  twoPair = true;
+	  }
+	  	  
+	  var flush = false;
+	  var flushArrayHolder = new Array(5);
+	  var flushArray = new Array(5);
+	  var flushSuit = -1;
+	  var count = 0;
+	  
+	  var straightHigh = -1;
+	  var straight = false;
+	  
+	  var straightFlush = false;
+	  var straightFlushHigh = -1;
+	  
+	  
+	  //Flush Section
+	  
+	  //Go through matrix 
+	  for (var i = 0; i < tableArray.length; i++) {
+		  for(var j = 0; j < tableArray[i].length; j++){	
+			    	  
+			  
+			  // 1 means we found a card
+			  if(tableArray[i][j] == 1){
+				  count = count + 1;		
+			  }
+			  
+			  // if 5 is reached that means we found a flush thus we must set which suit it is incase of a straight flush
+			  if(count >= 5){
+				  flushSuit = i;
+				  flush = true;
+			  }
+			  
+		  }
+		  count = 0;  
+	  }
+	  
+
+	  
+	  //Straight Section
+	  for(var k = 2; k < (matchArray.length - 2); k++){
+		  //checking 5 slots at a time
+		  if(matchArray[k-2] > 0 && matchArray[k-1] > 0 && matchArray[k] > 0 && matchArray[k+1] > 0 && matchArray[k+2] > 0){
+			  straight = true;
+			  straightHigh = k+2;
+		  }
+		 //checking in the case of A,2,3,4,5
+		  if(matchArray[0] > 0 && matchArray[1] > 0 && matchArray[2] > 0 && matchArray[3] > 0 && matchArray[12] > 0 && matchArray[4] == 0){
+			  straight = true;
+			  straightHigh = 3;
+		  }
+	  }
+	  
+
+	  
+	  //Straight flush Section
+	  if(straight && flush){
+		  
+		  //checking within the flush suit
+		  for(var k = 2; k < (tableArray[flushSuit].length - 2); k++){
+			  
+			  //checking 5 slots at a time
+			  if(tableArray[flushSuit][k-2] > 0 && tableArray[flushSuit][k-1] > 0 && tableArray[flushSuit][k] > 0 && tableArray[flushSuit][k+1] > 0 && tableArray[flushSuit][k+2] > 0){
+				  straightFlush = true;
+				  straightFlushHigh = k+2;
+			  }
+			  
+			  //checking in the case of A,2,3,4,5
+			  if(tableArray[flushSuit][0] > 0 && tableArray[flushSuit][1] > 0 && tableArray[flushSuit][2] > 0 && tableArray[flushSuit][3] > 0 && tableArray[flushSuit][12] > 0 && tableArray[flushSuit][4] == 0){
+				  straightFlush = true;
+				  straightFlushHigh = 3;
+			  }
+		  }
+		  
+	  }
+	  
+	  
+	  if(straightFlush){
+		  rArray[8] = 2;
+	  }if(fourCounter == 1){
+		  rArray[7] = 2;
+	  }if (fullHouse){
+		  rArray[6] = 2;
+	  }if(flush){
+		  rArray[5] = 2;
+	  }if(straight){
+		  rArray[4] = 2;
+	  }if (tripleCounter >= 1){
+		  rArray[3] = 2;
+	  }if (twoPair){
+		  rArray[2] = 2;
+	  }if(doubleCounter == 1){
+		  rArray[1] = 2;
+	  } 	  
 	 
 	 return rArray;
 	
